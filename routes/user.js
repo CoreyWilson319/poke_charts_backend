dc62cbc1e2be032f5ecdc2f03a87e8b2bc9ab232
Your Name <you@example.com>
2021-04-11 19:27:24 -0400

    [user.js] leaving off building get route

--- routes/user.js
+++ routes/user.js
@@ -1,9 +1,21 @@
 const router = require("express").Router();
 const auth = require("../middleware/auth");
+let User = require("../models/user.model");
+
 
 // GET
 // SHOW USER PROFILE
     // IF PROFILE OWNER HAS PROFILE PUBLIC SHOW USER POKEMON
+router.get("/:id", async (req, res) => {
+    try {
+        user = await User.findOne({ id: req.params.id});
+        if (user)
+        res.status(200).json(user)
+    }
+    catch(err) {
+        res.status(400).json({ msg: err})
+    }
+})
 
 
 module.exports = router;
